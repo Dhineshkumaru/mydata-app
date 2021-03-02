d789abc071c0d36fd9f069e9ac30a9aa64004129
dhineshkumaru <dhineshkumarugendran@gmail.com>
2021-03-02 09:25:49 +0530

    Sidebar changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { SidebarModule } from 'ng-sidebar'
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
