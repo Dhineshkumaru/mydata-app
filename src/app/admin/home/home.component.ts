175df160fc069b72894820d5f7932aa945dba133
dhineshkumaru <dhineshkumarugendran@gmail.com>
2021-03-07 08:33:24 +0530

    Structural directives accesing with array obeject.

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -6,8 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  // strArrs: Object[];
+  strArrs = [
+    {
+      strline:'these texts are working1',
+      strline1:'these texts are working2'
+    },
+    {
+      strline:'these texts are working3',
+      strline1:'these texts are working4'
+    },
+    {
+      strline:'these texts are working5',
+      strline1:'these texts are working6'
+    }
+  ];
+  
+  constructor() {
+    // this.strArrs = [
+    //   {
+    //     strline:'these texts are working1',
+    //     strline1:'these texts are working2'
+    //   },
+    //   {
+    //     strline:'these texts are working3',
+    //     strline1:'these texts are working4'
+    //   },
+    //   {
+    //     strline:'these texts are working5',
+    //     strline1:'these texts are working6'
+    //   }
+    // ];
+   }
 
   ngOnInit(): void {
   }
