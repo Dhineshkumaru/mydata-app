d549714bdab671f85782fecae268b64a0970fcee
dhineshkumaru <dhineshkumarugendran@gmail.com>
2021-03-07 10:46:22 +0530

    Hiden property used for ngFor structural dirctive.

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -9,16 +9,30 @@ export class HomeComponent implements OnInit {
   // strArrs: Object[];
   strArrs = [
     {
-      strline:'these texts are working1',
-      strline1:'these texts are working2'
+      strline:'these texts are working1-1',
+      strline1:'these texts are working1-2'
     },
     {
-      strline:'these texts are working3',
-      strline1:'these texts are working4'
+      strline:'these texts are working2-1',
+      strline1:'these texts are working2-2'
     },
     {
-      strline:'these texts are working5',
-      strline1:'these texts are working6'
+      strline:'these texts are working3-1',
+      strline1:'these texts are working3-2'
+    }
+  ];
+  strArrs1 = [
+    {
+      strline2:'these texts are working1-3',
+      strline3:'these texts are working1-4'
+    },
+    {
+      strline2:'these texts are working2-3',
+      strline3:'these texts are working2-4'
+    },
+    {
+      strline2:'these texts are working3-3',
+      strline3:'these texts are working3-4'
     }
   ];
   
