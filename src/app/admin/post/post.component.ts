d42661ae68911e794b8dd66e5bca907f497c64b0
dhineshkumaru <dhineshkumarugendran@gmail.com>
2021-03-08 22:38:20 +0530

    NgIf directive testing.

--- src/app/admin/post/post.component.ts
+++ src/app/admin/post/post.component.ts
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  people: any[] = [
+    {
+      "name": "Douglas  Pace",
+      "age": 35
+    },
+    {
+      "name": "Mcleod  Mueller",
+      "age": 32
+    },
+    {
+      "name": "Day  Meyers",
+      "age": 21
+    },
+    {
+      "name": "Aguirre  Ellis",
+      "age": 34
+    },
+    {
+      "name": "Cook  Tyson",
+      "age": 32
+    }
+  ];
 
   constructor() { }
 
